6cd2354b90e8a08ad25d88b164111631cc384147
antoabrilx <antoaldalla@gmail.com>
2025-10-28 09:13:37 -0300

    Creacion de carpeta data (movimientos+acciones)

---
+++ src/data/materiales.js
@@ -0,0 +1,8 @@
+export const initialMaterials = [
+    { id:1, name:'Arena fina para construcción', quantity:50 },
+    { id:2, name:'Hierro corrugado 12mm', quantity:25 },
+    { id:3, name:'Madera de pino 2x4', quantity:30 },
+    { id:4, name:'Martillo de carpintero', quantity:8 },
+    { id:5, name:'Destornillador Phillips', quantity:12 },
+    { id:6, name:'Cemento Portland', quantity:5 }
+];
