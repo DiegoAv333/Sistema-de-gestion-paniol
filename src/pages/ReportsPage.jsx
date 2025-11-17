7b2ef88b1abb26eb923a59e626e59eb5db5e8085
antoabrilx <antoaldalla@gmail.com>
2025-11-17 10:29:59 -0300

    Modificando estilo de boton

--- src/pages/ReportsPage.jsx
+++ src/pages/ReportsPage.jsx
@@ -137,7 +137,7 @@ return (
             <div className="md:col-span-1 flex items-end">
             <button
                 onClick={reset}
-                className="w-full inline-flex justify-center py-2 px-4 rounded-md border border-gray-300 bg-white hover:bg-gray-50 text-sm"
+                className="w-full inline-flex justify-center py-2 px-4 rounded-md border border-gray-200 bg-white hover:bg-blue-50 text-sm"
             >
                 Limpiar filtros
             </button>
